0287923d3e2c50a9a3cf07c6cf8ffd3b7a6a4599
Hellhawk95 <Desmangles95@gmail.com>
2018-02-07 19:07:47 -0600

    MG3 and 4 update
    -Prototyped MG4, super basic walls are in.
    -Added measures for the player to not get stuck in MG3 until the boxes are in.
    -Made the camera follow the player in MG3
    -The player can now pick up the shoes in Basement 4

---
+++ Of a Feather/Assets/Alex 'YSHLANtF' Prototype/Scripts/Specific RoomScripts/MG4_FriendMirrorScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MG4_FriendMirrorScript : MonoBehaviour {
+
+	GameObject playerObject;
+	Vector3 mirroredPlayerTransform;
+
+	// Use this for initialization
+	void Start () {
+
+		playerObject = GameObject.FindGameObjectWithTag("Player");
+		mirroredPlayerTransform = playerObject.transform.position;
+		mirroredPlayerTransform.x *= -1;
+		transform.position = mirroredPlayerTransform;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		mirroredPlayerTransform = playerObject.transform.position;
+		mirroredPlayerTransform.x *= -1;
+		transform.position = mirroredPlayerTransform;
+		
+	}
+}
