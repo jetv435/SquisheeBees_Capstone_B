d161fd1210279c8f81ecf1945a2012f4233b75ae
jetv435 <johnsoe2@yahoo.com>
2018-02-14 15:14:43 -0600

    Added WIP/placeholder MutexArrowNoRepeatComponent to the RhythmCore GameObjects in minigame 1, successfully tested with a debug print. horay

--- Of a Feather/Assets/Rhythm System/_Internal/BeatGenerationComponents/MutexArrowNoRepeatComponent.cs
+++ Of a Feather/Assets/Rhythm System/_Internal/BeatGenerationComponents/MutexArrowNoRepeatComponent.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CoordArrowNoRepeatA : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
+public class MutexArrowNoRepeatComponent : ABeatGenerationComponent
+{
+	void Start ()
+    {
+        // Update() should not be called on this component
+        this.enabled = false;	
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        Debug.LogError("Update() of MutexArrowNoRepeatComponent called - should not occur.");
 	}
+
+    public override ABeatGenerationStrategy GetStrategy()
+    {
+        // ToDo: Replace this with design-correct code (create Mutex strategies)
+        Debug.Log("HORAY: We have successfully provided a custom beat generation strategy to our RhythmCore!");
+        return new RandArrowNoRepeatStrat();
+    }
 }
