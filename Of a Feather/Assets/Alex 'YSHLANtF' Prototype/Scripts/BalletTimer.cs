b0f87b26aeadaeeb58f4827b07972c7d21ba9b6b
Diacono77 <davidi0610@gmail.com>
2018-02-19 17:41:07 -0600

    Eureka! Lights
    -Added in a light switch that allows players to toggle lights on and off.
    -Built lights to see it in editor.
    -Fixed text boxes to make them transparent.
    -Adjusted size of text but needs to be messed with based on the amount of dialogue
    for each item.
    -Built a timer for the ballet shoes but it is not currently part of the game due to:
    -----ballet shoes control fade manager so it gets wonky.

---
+++ Of a Feather/Assets/Alex 'YSHLANtF' Prototype/Scripts/BalletTimer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalletTimer : MonoBehaviour {
+
+
+	public int timer = 0;
+	public GameObject shoes;
+
+
+	// Use this for initialization
+	void Start () {
+		shoes.SetActive (false);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		timer--;
+		Debug.Log (timer);
+		if (timer <= 0) {
+
+			shoes.SetActive (true);
+			timer = 0;
+		}
+	}
+}
