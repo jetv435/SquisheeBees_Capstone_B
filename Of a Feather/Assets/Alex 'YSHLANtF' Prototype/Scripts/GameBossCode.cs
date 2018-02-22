271fdf45d5cfc326f15bdeccceada1cb22d868ad
jetv435 <johnsoe2@yahoo.com>
2018-02-22 15:53:23 -0600

    First-pass prototype of basement saving system.

--- Of a Feather/Assets/Alex 'YSHLANtF' Prototype/Scripts/GameBossCode.cs
+++ Of a Feather/Assets/Alex 'YSHLANtF' Prototype/Scripts/GameBossCode.cs
@@ -50,6 +50,41 @@ public class GameBossCode : MonoBehaviour {
 		else if (Input.GetKeyDown (KeyCode.Alpha0)) {
 			SceneManager.LoadScene ("Room_Minigame4");
 		}
+        else if(Input.GetKeyDown(KeyCode.O))
+        {
+            // save basement
+            BasementSaveSystem.SaveBasement("default_save", BasementSaveSystem.BasementSerialID.BASEMENT_BackToNormal);
+        }
+        else if(Input.GetKeyDown(KeyCode.P))
+        {
+            // load basement
+            BasementSaveSystem.Code saveRet = BasementSaveSystem.LoadBasement("default_save");
+
+            // check for any error return codes
+            if (saveRet != BasementSaveSystem.Code.SUCCESS)
+            {
+                switch (saveRet)
+                {
+                    case BasementSaveSystem.Code.SAVE_FILE_NOT_FOUND:
+                        {
+                            Debug.LogError("Save file not found in LoadBasement()");
+                        } break;
+                    case BasementSaveSystem.Code.VERSION_MISMATCH:
+                        {
+                            Debug.LogError("Save file version mismatch in LoadBasement()");
+                        } break;
+                    case BasementSaveSystem.Code.INVALID_FILE_DATA:
+                        {
+                            Debug.LogError("Invalid save file data in LoadBasement()");
+                        } break;
+                    default:
+                        {
+                            Debug.LogError("Unexpected error returned from LoadBasement()");
+                        } break;
+                }
+            }
+
+        }
 	}
 
 	public void GoToBasement(int RoomNum)
