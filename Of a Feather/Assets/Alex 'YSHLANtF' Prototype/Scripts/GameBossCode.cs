8129bb4c81ddae2548b39c63842a8be8169e1771
Diacono77 <davidi0610@gmail.com>
2018-03-07 23:01:45 -0600

    Another fun update
    -Added in Main menu and instruction screens.

--- Of a Feather/Assets/Alex 'YSHLANtF' Prototype/Scripts/GameBossCode.cs
+++ Of a Feather/Assets/Alex 'YSHLANtF' Prototype/Scripts/GameBossCode.cs
@@ -91,4 +91,6 @@ public class GameBossCode : MonoBehaviour {
 	{
 		StartCoroutine (MM.Transition ());
 	}
+
+
 }
---
+++ Of a Feather/Assets/Alex 'YSHLANtF' Prototype/Scripts/GameManagers/MenuManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+	MainMenuManager MM;
+
+	// Use this for initialization
+	void Start () {
+		//MM = GameObject.FindGameObjectWithTag ("MManager").GetComponent<MainMenuManager> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Quit ();
+		}
+		
+	}
+
+	public void Instructions(){
+
+		SceneManager.LoadScene ("Instructions");
+
+	}
+
+	public void goPlay()
+	{
+
+		SceneManager.LoadScene ("Basement0");
+		//StartCoroutine (MM.Transition ());
+	}
+
+	public void MainMenu()
+	{
+		SceneManager.LoadScene ("MainMenu");
+
+	}
+
+	public void Quit()
+	{
+		Application.Quit ();
+	}
+}
