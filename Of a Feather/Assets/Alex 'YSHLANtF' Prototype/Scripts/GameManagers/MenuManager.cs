783489d24aaf9a7acf8f59385d4437ce071d2189
Diacono77 <davidi0610@gmail.com>
2018-03-08 03:03:00 -0600

    Minor Update
    -Fixed bug in last basement where you would be teleported back to the same room.
    -Also changed keycode to quit out of the application. it is on F1.

--- Of a Feather/Assets/Alex 'YSHLANtF' Prototype/Scripts/GameManagers/MenuManager.cs
+++ Of a Feather/Assets/Alex 'YSHLANtF' Prototype/Scripts/GameManagers/MenuManager.cs
@@ -15,7 +15,7 @@ public class MenuManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.GetKeyDown (KeyCode.Escape)) {
+		if (Input.GetKeyDown (KeyCode.F1)) {
 			Quit ();
 		}
 		
