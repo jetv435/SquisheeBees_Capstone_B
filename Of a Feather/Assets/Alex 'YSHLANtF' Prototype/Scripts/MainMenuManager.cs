b3c7dfb7fc1db878712a27eefb82d40106a92506
Diacono77 <davidi0610@gmail.com>
2018-05-25 23:11:00 -0500

    Minor Fade
    -adjusted fade timing to better allow players to read the text on screen.
    -created a fade in for the text for polish.
    -need to seperate 2d fades from 3d!

--- Of a Feather/Assets/Alex 'YSHLANtF' Prototype/Scripts/MainMenuManager.cs
+++ Of a Feather/Assets/Alex 'YSHLANtF' Prototype/Scripts/MainMenuManager.cs
@@ -8,6 +8,7 @@ public class MainMenuManager : MonoBehaviour {
 	private FadeManager fm;
 	public CanvasGroup FadeInOutPanel;
 	public CanvasGroup Introfade;
+	public CanvasGroup texter;
 	//public int previous;
 	public int nextroom;
 	public AudioSource ambient;
@@ -47,24 +48,27 @@ public class MainMenuManager : MonoBehaviour {
 		SceneManager.LoadScene (nextroom);
 	}
 
-
+	//loading in to new room
 	private IEnumerator Fadeout(){
 		fm.FadeOut (Introfade, 30f, 1f);
-		ambient.PlayDelayed (4.75f);
+		ambient.PlayDelayed (15.75f);
 		ambient1.mute = true;
-		yield return new WaitForSeconds (5f);
+		fm.FadeIn (texter, 10f, 1f);
+		yield return new WaitForSeconds (15f);
 		fm.FadeOut (Introfade, 1f);
 		yield return new WaitForSeconds (1f);
 		ambient1.mute = false;
 	}
 
+	//moves to next room
 	public IEnumerator SGCorutine(){
 		//yield return new WaitForSeconds (3f);
 		fm.FadeIn (FadeInOutPanel, 60f, 1f);
 		yield return new WaitForSeconds (1f);
 		ambient.Stop ();
+		fm.FadeIn (texter, 60f, 1f);
 		fm.FadeIn (FadeInOutPanel, 120f, 1f);
-		yield return new WaitForSeconds (5f);
+		yield return new WaitForSeconds (10f);
 		SceneManager.LoadScene (nextroom);
 	}
 
